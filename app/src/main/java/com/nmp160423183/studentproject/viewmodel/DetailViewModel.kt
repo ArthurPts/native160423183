86ddcc70af8371be0e737cb7aa4dfe60a097c5e2
ArthurPts <141316872+ArthurPts@users.noreply.github.com>
2026-04-08 08:31:01 +0700

    update DetailViewModel.kt

--- app/src/main/java/com/nmp160423183/studentproject/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/nmp160423183/studentproject/viewmodel/DetailViewModel.kt
@@ -1,18 +1,31 @@
 package com.nmp160423183.studentproject.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.nmp160423183.studentproject.model.Student
 
-class DetailViewModel: ViewModel() {
+class DetailViewModel(application: Application): AndroidViewModel(application) {
     val studentLD = MutableLiveData<Student>()
+    val TAG = "Volley Tag"
+    var queue: RequestQueue?=null
 
     fun fetch(student: Student){
 //        val student1 = Student("16055","Nonie","1998/03/28","5718444778",
 //            "http://dummyimage.com/75x100.jpg/cc0000/ffffff")
 //        studentLD.value = student1
-
+        queue = Volley.newRequestQueue(getApplication())
+        val url = "https://www.jsonkeeper.com/b/LLMW"
+        val stringreq = StringRequest(Request.Method.GET, url, {},{})
+        stringreq.tag = TAG
+        queue?.add(stringreq)
         studentLD.value = student
+
 
 
     }
